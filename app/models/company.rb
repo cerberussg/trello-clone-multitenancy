97aa854b20f6575619a5d447328683c1a0f2bb33
cerberussg <scott.goyette@rubypythmore.com>
2023-01-22 21:27:06 -0500

    model setup

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
+# Company model
 class Company < ApplicationRecord
+  has_many :users, dependent: :destroy
+  has_many :lists, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
+# Users model
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   belongs_to :company
+  accepts_nested_attributes_for :company, reject_if: ->(attributes) { attributes["name"].blank? }, allow_destroy: true
 end
